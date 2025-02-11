c98739f0601d4271ebacb62ef9a3a9c458ba95a2
Michael Hudson <michaelhudson2205@gmail.com>
2025-02-11 20:09:52 +1100

    Up to Chapter 1 Blocking.

--- Chapter1.R
+++ Chapter1.R
@@ -1,2 +1,12 @@
-library(readr)
-write_csv(iris, "data-raw.csv")
+library(tidyverse)
+library(broom)
+
+data("ToothGrowth")
+head(ToothGrowth)
+
+ToothGrowth |> 
+  summarise(mean(len),
+            median(len),
+            sd(len))
+
+t.test(x = ToothGrowth$len, alternative = "two.sided", mu = 18)
